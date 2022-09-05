f0f1667b235e1fbce65c30b0c653c0fbb5e14700
lerrybe <lerry597@gmail.com>
2022-09-05 17:43:38 +0900

    feat: 더미 데이터 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,41 +4,14 @@ import Gnb from "../components/gnb";
 import MenuList from "../components/menu-list";
 import MenuDetail from "../components/menu-detail";
 
-const MenuManagePage = () => {
-  const dummyArr = [
-    {
-      id: 1,
-      name: "초코와플",
-      price: 7000,
-      image: "",
-    },
-    {
-      id: 2,
-      name: "아메리카노",
-      price: 4000,
-      image: "",
-    },
-    {
-      id: 3,
-      name: "블루베리스무디",
-      price: 6000,
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/1/15/Blueberries.jpg",
-    },
-    {
-      id: 4,
-      name: "딸기와플",
-      price: 7000,
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/29/PerfectStrawberry.jpg",
-    },
-  ];
+import { menu } from "../data/dummy";
 
+const MenuManagePage = () => {
   return (
     <>
       <Gnb />
       <div className="page-content-wrapper">
-        <MenuList menuItems={dummyArr} />
+        <MenuList menuItems={menu} />
         <MenuDetail />
       </div>
     </>
