28b4923b01b6001f17d49ce1e09f7d0de54b1f34
lerrybe <lerry597@gmail.com>
2022-11-01 01:24:12 +0900

    fix: set proxy (bug)

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,9 +14,15 @@ export const requestLogin = async ({ name, password }) => {
   }
 };
 
-export const requestLogout = async () => {
+// BUG: fix logout 401
+export const requestLogout = async (accessToken) => {
   try {
-    await axios.post(`${BASE_URL}/auth/logout`, {});
+    await axios.post(process.env.NODE_ENV === 'development' ? '/auth/logout' : `${BASE_URL}/auth/logout`, null, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
   } catch (err) {
     console.log(err);
   }
