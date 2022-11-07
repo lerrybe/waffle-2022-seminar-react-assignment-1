8ceadb589a97dac4f49a18c43c865a7eca209a10
lerrybe <lerry597@gmail.com>
2022-11-07 20:07:22 +0900

    feat: 에러 페이지 alert 교체

--- src/pages/error-page/index.js
+++ src/pages/error-page/index.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { toast } from 'react-toastify';
+
 function ErrorPage() {
   useEffect(() => {
-    alert('올바르지 않은 주소입니다.');
+    toast.error('올바르지 않은 접근입니다.');
   }, []);
 
   return <Navigate to={-1} />;
