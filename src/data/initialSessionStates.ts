7b79fe1c7407d8140058950a2f7af766c9096796
lerrybe <lerry597@gmail.com>
2022-11-13 11:53:08 +0900

    refactor: context 타입 수정

---
+++ src/data/initialSessionStates.ts
@@ -0,0 +1,27 @@
+import { LoginRequest, Owner } from '../types/auth';
+
+// about context states
+export interface State {
+  user?: Owner | null;
+  accessToken?: string | null;
+}
+
+export const initialUser: State = {
+  user: null,
+  accessToken: null,
+};
+
+// about dispatch states
+export interface Dispatches {
+  login?: ({ username, password }: LoginRequest) => Promise<void>;
+  logout?: (accessToken: string) => Promise<void>;
+}
+
+export const initialUserActions: Dispatches = {
+  login() {
+    throw new Error('SessionContext not provided');
+  },
+  logout() {
+    throw new Error('SessionContext not provided');
+  },
+};
