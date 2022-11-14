fb001656c51a39bc4f167011ddd5cd5f0d6ad761
lerrybe <lerry597@gmail.com>
2022-11-14 17:38:51 +0900

    refactor: store card 리팩토링 with TS & emotion

---
+++ src/components/store-card/index.tsx
@@ -0,0 +1,39 @@
+import { Rating } from '@mui/material';
+import {
+  Wrapper,
+  Username,
+  Storename,
+  StoreDescription,
+} from './store-card.styled';
+
+interface StoreCard {
+  storeName: string;
+  username: string;
+  storeDesc: string;
+  rating: number;
+  handleClick: () => void;
+}
+
+function StoreCard({
+  storeName,
+  username,
+  storeDesc,
+  rating,
+  handleClick,
+}: StoreCard) {
+  return (
+    <Wrapper onClick={handleClick}>
+      <Storename>{storeName}</Storename>
+      <Username>{username}</Username>
+      <StoreDescription>{storeDesc}</StoreDescription>
+      <Rating
+        name="half-rating-read"
+        value={Number((rating / 2).toFixed(1))}
+        precision={0.5}
+        readOnly
+      />
+    </Wrapper>
+  );
+}
+
+export default StoreCard;
